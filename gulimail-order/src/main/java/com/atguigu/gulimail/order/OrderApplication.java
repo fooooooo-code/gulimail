4432ff8ac369872c5d9f24bbfb24c5f9c38f82ce
test <test@163.com>
2024-02-08 23:12:32 +0800

    init

---
+++ gulimail-order/src/main/java/com/atguigu/gulimail/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
